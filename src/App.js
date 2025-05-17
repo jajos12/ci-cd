8a8aad6a729a7cafd0a2cda77aee7a69b2468408
Yonatan Getachew <yonatangetachew91@gmail.com>
2025-05-17 13:17:52 +0300

    intentionally messed up to fail  the test

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
               </div>
               <div className="flex items-center">
                 <button
-                  onClick={handleLogout}
+                  onClick={handlLogout}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-md text-sm font-medium transition duration-150 ease-in-out"
                 >
                   Logout
